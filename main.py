550711bf9879e850067ad414f2a9cba161440166
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-11-06 17:42:49 -0300

    create function show_search_page

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello world")
+import flet as ft
+
+
+def main(page: ft.Page):
+    def show_search_page(event=None):
+        page.clean()
+
+        search_bar = ft.TextField(prefix_icon=ft.icons.SEARCH, hint_text="Pesquisar")
+        layout = ft.Column(expand=True, controls=[search_bar])
+
+        page.add(layout)
+
+    show_search_page()
+
+
+ft.app(target=main, view=ft.AppView.FLET_APP)
